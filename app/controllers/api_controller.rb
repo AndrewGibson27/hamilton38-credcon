c647856f30a8aaa853d8011f8cae65ce80f2dacf
Scott Hudson <scott.w.hudson@gmail.com>
2018-11-17 15:54:54 -0600

    serialize resources

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,15 +1,35 @@
 class ApiController < ApplicationController
 
   def index
-    data = {
-      status: response.status,
+    response = {
+      status: status,
       url: api_params[:url]
-    }
+    }.merge(serialized_resource)
 
-    render json: data
+    render json: response
   end
 
   private
+
+  def resource
+    @resource ||= Resource.find_by(path: api_params[:url])
+  end
+
+  def status
+    if serialized_resource
+      200
+    else
+      404
+    end
+  end
+
+  def serialized_resource
+    @serialized_resource ||= if resource
+      ResourceSerializer.new(resource).serializable_hash
+    else
+      nil
+    end
+  end
 
   def api_params
     params.permit(:url)
---
+++ app/serializers/resource_serializer.rb
@@ -0,0 +1,4 @@
+class ResourceSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :path
+end
